73af5b41b1d5a2c4371fd72c237668ba63e7dcab
FRED <fredlauray@gmail.com>
2019-10-30 19:38:43 -0500

    feat: se hace buscar actualizar y eliminar producto

--- desarrollo/SIRP/codigofuente/sirp_back/app/Http/Controllers/ProductController.php
+++ desarrollo/SIRP/codigofuente/sirp_back/app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\User;
+use http\Env\Response;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,8 +14,6 @@ class ProductController extends Controller
     {
         $request->limit? $p = $request->limit : $p = 10;
         $request->order? $order = $request->order : $order = 'desc';
-        $user = TokenController::getUserbyToken($request->header('token'));
-        $usr = User::find($user->user_id);
 
         $products = Product::with('category','brand')
             ->orderBy('created_at',$order)
@@ -82,4 +81,131 @@ class ProductController extends Controller
         }
         return response()->json($res, $this->status);
     }
+
+    public function show(Request $request)
+    {
+        if(!$request->search)
+        {
+            $res = [
+                'success' => 0,
+                'msg' => 'Parametro search no recibido'
+            ];
+            $this->status = 500;
+        }
+        else
+        {
+            $search = '%'.$request->search.'%';
+            $products = Product::with('category','brand')
+                ->where('product_name','like',$search)
+                ->orwhere('product_code','like',$search)
+                ->orderBy('created_at','asc')
+                ->get();
+
+            $res = [
+                'success' => 1,
+                'response' => $products
+            ];
+        }
+        return response()->json($res ,$this->status);
+    }
+
+    public function update(Request $request)
+    {
+        if(!$request->product_id)
+        {
+            $res = [
+                'success' => 0,
+                'msg' => 'Parametro product_id no recibido'
+            ];
+            $this->status = 500;
+        }
+        else
+        {
+            $product = Product::where('product_id', $request->product_id)->first();
+
+            if($product)
+            {
+                if (!empty($request->product_code)){
+                    $code = Product::where('product_code', $request->product_code)->first();
+                    if($code)
+                    {
+                        $res = [
+                            'success' => 0,
+                            'msg' => 'Ya existe un producto registrado con el codigo'.$request->product_code
+                        ];
+                        return response()->json($res,500);
+                    }
+                    else
+                    {
+                        $product->product_code = $request->product_code;
+                    }
+
+                }
+                if (!empty($request->product_name)) {
+                    $product->product_name = $request->product_name;
+                }
+                if (!empty($request->category)) {
+                    $product->category_id = $request->category;
+                }
+                if (!empty($request->brand_id)) {
+                    $product->brand_id = $request->brand;
+                }
+                if (!empty($request->price)) {
+                    $product->price = $request->price;
+                }
+
+                $product->update();
+
+                $res = [
+                    'success' => 1,
+                    'msg' => 'Producto Actualizado con Exito'
+                ];
+            }
+            else
+            {
+                $res = [
+                    'success' => 0,
+                    'msg' => 'Producto no encontrado'
+                ];
+                $this->status = 500;
+            }
+        }
+        return response()->json($res);
+    }
+
+    public function destroy(Request $request)
+    {
+        if(!$request->product_id)
+        {
+            $res = [
+                'success' => 0,
+                'msg' => 'Parametro product_id no recibido'
+
+            ];
+            $this->status = 500;
+        }
+        else
+        {
+            $product = Product::where('product_id',$request->product_id)->first();
+
+            if($product){
+                $product->delete();
+                $res = [
+                    'success' => 1,
+                    'msg' => 'Producto eliminado con exito'
+                ];
+            }
+            else
+            {
+                $res = [
+                    'success' => 0,
+                    'msg' => 'Producto no encontrado'
+                ];
+                $this->status = 500;
+            }
+        }
+        return response()->json($res,$this->status);
+    }
+
+
 }
--- desarrollo/SIRP/codigofuente/sirp_back/database/migrations/2019_10_30_041207_create_products_table.php
+++ desarrollo/SIRP/codigofuente/sirp_back/database/migrations/2019_10_30_041207_create_products_table.php
@@ -19,7 +19,7 @@ class CreateProductsTable extends Migration
             $table->string('product_name');
             $table->integer('category_id');
             $table->integer('brand_id');
-            $table->integer('price');
+            $table->float('price');
             $table->timestamps();
             $table->softDeletes();
         });
