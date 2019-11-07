8358084148868c853d097d08c1ca0c959f2dd447
DiazC <loguepw@gmail.com>
2019-11-06 21:46:44 -0500

    refactor boilerplate

--- desarrollo/SIRP/codigofuente/sirp-front/src/App.vue
+++ desarrollo/SIRP/codigofuente/sirp-front/src/App.vue
@@ -1,36 +1,58 @@
 <template>
-  <main>
-    <transition mode="out-in">
-      <router-view/>
-    </transition>
-  </main>
+  <div class="app-root">
+    <router-view></router-view>
+    <!-- theme setting -->
+    <v-btn small fab dark falt fixed top="top" right="right" class="setting-fab" color="red" @click="openThemeSettings">
+      <v-icon>settings</v-icon>
+    </v-btn>
+    <!-- setting drawer -->
+    <v-navigation-drawer class="setting-drawer" temporary right v-model="rightDrawer" hide-overlay fixed>
+      <theme-settings></theme-settings>
+    </v-navigation-drawer>
+    <!-- global snackbar -->
+    <v-snackbar :timeout="3000" bottom right :color="snackbar.color" v-model="snackbar.show">
+      {{ snackbar.text }}
+      <v-btn dark flat @click.native="snackbar.show = false" icon>
+        <v-icon>close</v-icon>
+      </v-btn>
+    </v-snackbar>
+  </div>
 </template>
 
-<style lang="scss">
-@import "@/styles/index.scss";
-
-/* Remove in 1.2 */
-.v-datatable thead th.column.sortable i {
-  vertical-align: unset;
-}
-</style>
 <script>
-// checks to see if auth jwt token is valid or has expired, if it gets back 401 error log out
+import ThemeSettings from "@/components/ThemeSettings"
+import AppEvents from "./event"
+
 export default {
-  created: function () {
-    this.$http.interceptors.response.use((response) => {
-      return response
-    }, (error) => {
-      if (error.response.status === 401) {
-        if (this.$store.getters.authorized) {
-          this.$store.dispatch('refreshtoken')
-        } else {
-          return Promise.reject(error)
-        }
-      } else {
-        return Promise.reject(error)
+  components: {
+    ThemeSettings
+  },
+  data() {
+    return {
+      rightDrawer: false,
+      snackbar: {
+        show: false,
+        text: "",
+        color: ""
       }
-    })
+    }
+  },
+  created() {
+    // add app events
+  },
+  methods: {
+    openThemeSettings() {
+      this.$vuetify.goTo(0)
+      this.rightDrawer = !this.rightDrawer
+    }
   }
 }
 </script>
+
+<style scoped>
+.setting-fab {
+  top: 50% !important;
+  right: 0;
+  border-radius: 0;
+}
+</style>
