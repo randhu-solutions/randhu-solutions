04bcd66b093b554449bae08e0b047b5a08206506
aLopezVega <anthodev1195@gmail.com>
2019-12-09 23:25:22 -0500

    config index endpoint

--- desarrollo/SIRP/codigofuente/srip_IA/app.py
+++ desarrollo/SIRP/codigofuente/srip_IA/app.py
@@ -5,3 +5,14 @@ import numpy as np
 from random import randrange
 from flask import Flask, request, jsonify
 import json
+
+ds = pd.read_csv("products.csv", encoding='utf-8')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
